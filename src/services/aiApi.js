9a8cd9247e5f8f9621289117a2e8b2ebdf140764
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2025-01-18 22:14:08 +0800

    add api key

--- src/services/aiApi.js
+++ src/services/aiApi.js
@@ -1,6 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai"
 
-export const genAI = new GoogleGenerativeAI("YOUR_API_KEY");
+// eslint-disable-next-line no-undef
+export const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 export const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export const prompt = "Explain how AI works";
