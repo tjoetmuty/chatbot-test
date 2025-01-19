11022539932d70dcd66e4b63a53b2afe8babe200
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2025-01-19 20:56:54 +0800

    using markdown at the answer

--- src/features/ChatBot.jsx
+++ src/features/ChatBot.jsx
@@ -1,18 +1,22 @@
 import { useState } from "react";
 import { model } from "../services/aiApi";
+import Markdown from "react-markdown";
 
 const ChatBot = () => {
   const [userInput, setuserInput] = useState("");
   const [answer, setAnswer] = useState("")
+
   const handleUserInput = (e) => {
     setuserInput(e.target.value);
   };
+
   const modelAi = model
   const sendMessage = async () => {
     try {
       const result = await modelAi.generateContent(userInput);
       const aiAnswer = result.response.text()
       setAnswer(aiAnswer)
+      setuserInput("")
       console.log("hasil", aiAnswer);
     } catch(err) {
       console.log("apa yang terjadi", err.message)
@@ -38,7 +42,7 @@ const ChatBot = () => {
               Send
             </button>
           </div>
-          <div>{answer}</div>
+          <Markdown>{answer}</Markdown>
         </div>
       </div>
     </div>
